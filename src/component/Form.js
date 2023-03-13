322b46ab141eed7513af9ff0d3c1d0cc19dad36b
harsh sajla <harsh.sajla2001@gmail.com>
2023-03-13 17:47:23 +0530

    added styling

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,4 +1,4 @@
-import { TextField, Box, Grid, Button } from '@mui/material';
+import { TextField, Box, Grid, Button, Paper } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import SystemUpdateAltIcon from '@mui/icons-material/SystemUpdateAlt';
 import List from './List';
@@ -15,24 +15,30 @@ function Form() {
     // Function to export functions and variables to the parant component
     useEffect(() => {
         getValues({ editId, setEditId, setInputVal, inputVal, setShowBtn, showBtn })
-    }, [editId])
+    }, [editId, showBtn, inputVal, getValues])
     return (
         <form>
-            <Box sx={{ border: "solid red 2px" }}>
-                <Grid m={2} p={4} container gap={2} direction="column" justifyContent="center" alignItems="center" sx={{ border: "solid black 2px" }}>
-                    <TextField value={inputVal} onChange={(e) => setInputVal(e.target.value)} id="outlined-basic" label="Enter Here" variant="outlined" required sx={{ minWidth: '30%' }} autoFocus={true} />
-                    <Button onClick={() => {
-                        postTodo(inputVal)
-                        setShowBtn(false)
-                        setInputVal('')
-                    }} startIcon={<AddIcon fontSize='small' />} variant="outlined" sx={{ minWidth: '8%' }}>Add</Button>
-                    {showBtn && <Button onClick={() => {
+            <Box>
+                <Grid m={1} p={4} pb={1} mb={0} container gap={2} direction="row" justifyContent="center" alignItems="center" >
+                    <Paper elevation={3} >
+                        <TextField value={inputVal} onChange={(e) => setInputVal(e.target.value)} id="outlined-basic" label="Enter Here" variant="outlined" required sx={{ minWidth: '30%' }} autoFocus={true} />
+                    </Paper>
+                    <Paper elevation={3} >
+
+                        <Button onClick={() => {
+                            postTodo(inputVal)
+                            setShowBtn(false)
+                            setInputVal('')
+                        }} startIcon={<AddIcon fontSize='small' />} variant="outlined" sx={{ minWidth: '8%' }}>Add</Button>
+                    </Paper>
+                    {showBtn && <Paper elevation={3} > <Button onClick={() => {
                         editTodo(editId, inputVal)
                         setShowBtn(false)
                         setInputVal('')
-                    }} startIcon={<SystemUpdateAltIcon fontSize='small' />} variant="outlined" sx={{ minWidth: '8%' }}>Update</Button>}
-                    <List />
+                    }} startIcon={<SystemUpdateAltIcon fontSize='small' />} variant="outlined" sx={{ minWidth: '8%' }}>Update</Button></Paper>}
+
                 </Grid>
+                <Grid m={1} p={4} mt={0} container gap={2} direction="column" justifyContent="center" alignItems="center" ><List /></Grid>
             </Box>
         </form>
     )
--- src/component/List.js
+++ src/component/List.js
@@ -1,5 +1,5 @@
-import React, { useContext} from 'react';
-import { Grid, Box, Typography, Divider } from '@mui/material';
+import React, { useContext } from 'react';
+import { Grid, Box, Typography, Divider, Paper } from '@mui/material';
 import { GlobalInfo } from '../App';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
@@ -7,26 +7,30 @@ import EditIcon from '@mui/icons-material/Edit';
 function List() {
   const { todos, deletTodo, val } = useContext(GlobalInfo)
   return (
-    <Grid sx={{ border: "solid orange 2px" }} minWidth='30%' >
+    <Grid minWidth='30%' >
       {todos.map((todos, i) => {
-        return (<Grid key={i} sx={{ border: "solid purple 2px", display: 'flex', justifyContent: 'space-between' }} >
-          <Box sx={{ display: 'flex' }}>
-            <Typography p={2} >{i + 1}</Typography><Divider orientation="vertical" flexItem />
-            <Typography p={2} sx={{ display: 'flex' }}>{todos.todo}  </Typography>
-          </Box>
-          <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            <Divider orientation="vertical" flexItem />
-            <DeleteIcon onClick={() => {
-              deletTodo(todos.id)
-            }} sx={{ marginRight: "2rem", marginLeft: '1rem' }} />
-            <EditIcon onClick={() => {
-              val.setEditId(todos.id)
-              val.setInputVal(todos.todo)
-              val.setShowBtn(true)
-              
-            }} sx={{ marginRight: "1rem" }} />
-          </Box>
-        </Grid>)
+        return (
+          <Paper key={i} elevation={4}>
+            <Grid m={2} sx={{ display: 'flex', justifyContent: 'space-between' }} >
+              <Box sx={{ display: 'flex' }}>
+                <Typography p={2} >{i + 1}</Typography><Divider orientation="vertical" flexItem />
+                <Typography p={2} sx={{ display: 'flex' }}>{todos.todo}  </Typography>
+              </Box>
+              <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <Divider orientation="vertical" flexItem />
+                <DeleteIcon onClick={() => {
+                  deletTodo(todos.id)
+                }} sx={{ marginRight: "2rem", marginLeft: '1rem' }} />
+                <EditIcon onClick={() => {
+                  val.setEditId(todos.id)
+                  val.setInputVal(todos.todo)
+                  val.setShowBtn(true)
+
+                }} sx={{ marginRight: "1rem" }} />
+              </Box>
+            </Grid>
+          </Paper>
+        )
       })}
     </Grid>
   )
